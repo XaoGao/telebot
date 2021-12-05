31693b2cee0cd52699ae80c8b8d6a436bb5bc754
Rishat <81845@mail.ru>
2021-12-05 23:43:25 +0300

    Fix cli commands name

--- lib/generators/cli.rb
+++ lib/generators/cli.rb
@@ -5,23 +5,23 @@ module Telebot
     class Cli < Thor
       include Thor::Actions
 
-      desc 'command FILE_NAME', 'Create a new file in dir commands'
-      def command(file_name)
+      desc 'create_command FILE_NAME', 'Create a new file in dir commands'
+      def create_command(file_name)
         puts "Create #{file_name}"
       end
 
-      desc 'action FILE_NAME', 'Create a new file in dir actions'
-      def action(file_name)
+      desc 'create_action FILE_NAME', 'Create a new file in dir actions'
+      def create_action(file_name)
         puts "Create #{file_name}"
       end
 
-      desc 'model FILE_NAME', 'Create a new file in dir models'
-      def model(file_name)
+      desc 'create_model FILE_NAME', 'Create a new file in dir models'
+      def create_model(file_name)
         puts "Create #{file_name}"
       end
 
-      desc 'migration FILE_NAME', 'Create a new file in dir migrations'
-      def migration(file_name)
+      desc 'create_migration FILE_NAME', 'Create a new file in dir migrations'
+      def create_migration(file_name)
         puts "Create #{file_name}"
       end
     end
