3d2a18157376a997b1ec83e063bd264b86ec90c7
Rishat <81845@mail.ru>
2021-12-07 10:13:17 +0300

    add cli generate create migration

--- lib/generators/cli.rb
+++ lib/generators/cli.rb
@@ -17,7 +17,7 @@ module Telebot
         when 'action'
           create_action(name)
         when 'migration'
-          say "Create #{thing}", :green
+          create_migration(name)
         else
           say "Undefind #{thing} params, abort!", :red
         end
@@ -61,25 +61,31 @@ module Telebot
         end
       end
 
-      def create_model
-        
+      def create_model; end
+
+      def create_migration(file_name)
+        path = File.join('app', 'db', 'migrations')
+        existed_migrations = Dir["#{path}/*.rb"].sort
+        last_migration = existed_migrations.last.split('/').last
+        current_number_of_migration = next_number_of_migration(last_migration)
+        migration_name = File.join(path, [current_number_of_migration, "#{file_name}.rb"].join('_'))
+        create_file migration_name do
+          <<~MIGRATION
+            Sequel.migration do
+              up do
+
+              end
+
+              down do
+
+              end
+            end
+          MIGRATION
+        end
       end
 
-      def create_migration
-        path = File.join('app', 'db', 'migrations')
-        full_name = File.join(path, "#{file_name}.rb")
-        name = file_name.split('/').last
-        create_file full_name do
-          <<~COMMAND
-            class #{name.to_camel_case}Action < ApplicationCommand
-              # before_call :some_method
-              # try         :some_method
-              # when_error  :some_method
-              # finally     :some_method
-              # after_call  :some_method
-            end
-          COMMAND
-        end
+      def next_number_of_migration(migration)
+        migration.nil? ? '001' : format('%03d', (migration.split('_').first.to_i + 1))
       end
     end
   end
